b01d84b4f2936f5d42d964997cee8d189535c79b
Aryeh Beitz <aryehbeitz@gmail.com>
2025-03-19 10:10:50 +0200

    record name

--- record_video.sh
+++ record_video.sh
@@ -13,7 +13,11 @@ cleanup() {
 # Trap Ctrl+C (SIGINT) to run cleanup()
 trap cleanup SIGINT
 
-VIDEO_FILE="$WATCH_DIR/recording_$(date +%Y%m%d_%H%M%S).mov"
+# Get the start timestamp
+START_TIMESTAMP=$(date +"%Y_%m_%d_%H_%M_%S")
+END_SECOND=$(date -j -v+30S +"%S")  # Adds 30 seconds
+
+VIDEO_FILE="$WATCH_DIR/recording_${START_TIMESTAMP}-${END_SECOND}.mov"
 
 # Use Main Camera (Device Index 0) for recording with correct pixel format (silent)
 ffmpeg -f avfoundation -framerate 30 -video_size 1920x1080 -pixel_format uyvy422 -i "$MAIN_CAM_INDEX" -t 30 "$VIDEO_FILE" -loglevel error -nostats 2>/dev/null
